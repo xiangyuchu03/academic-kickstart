1a63f858e7d12dd52b5140d72d1ae2209afdf81f
Woooooo <wujiahao9494@gmail.com>
2020-05-30 12:58:03 -0400

    Update view.sh

--- view.sh
+++ view.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 hugo --i18n-warnings server
+
+<script type="text/javascript" src="//rf.revolvermaps.com/0/0/7.js?i=5p8mi02rowh&amp;m=0&amp;c=ff0000&amp;cr1=ffffff&amp;sx=0" async="async"></script>
